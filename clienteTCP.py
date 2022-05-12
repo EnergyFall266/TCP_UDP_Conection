4fdaf99aa8d5879166d88fbbb1f81ca76ab7a1e0
leonardo <leonardo.vanzin@hotmail.com>
2022-05-12 14:44:15 -0300

    TCP

--- clienteTCP.py
+++ clienteTCP.py
@@ -2,10 +2,24 @@ import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
+FILE_LEN_BYTES = 104857600
+PACKET_SIZE = 500
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+def clienteTCP(host, port, packetSize, file_len):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        data_sent = 0
+        #manda pacote inicial para indicar que o cronometro deve iniciar
+        s.send(b"-"*packetSize)
 
-print(f"Received {data!r}")
+        #envia dadodos enquanto o contador de dados for menor que 100MB
+        while(data_sent < file_len):
+            if((file_len-data_sent) < packetSize):
+                s.send(b"-"*(file_len-data_sent))    
+            else:
+                s.send(b"-"*packetSize)
+            data_sent += packetSize
+    print("All data sent")
+
+if __name__ == "__main__":
+    clienteTCP(HOST, PORT, PACKET_SIZE, FILE_LEN_BYTES)
--- serverTCP.py
+++ serverTCP.py
@@ -1,16 +1,38 @@
 import socket
+import time
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+PACKET_SIZE = 500
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
+def servidorTCP(host, port, packetSize):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen()
+        conn, addr = s.accept()
+        data_received = 0
         print(f"Connected by {addr}")
+
+        #espera pelo primeiro pacote para começar a contar
+        tempo_inicial = 0
         while True:
-            data = conn.recv(1024)
-            if not data:
+            data = conn.recv(packetSize)
+            if data:
+                print(data)
+                tempo_inicial = time.time()
                 break
-            conn.sendall(data)
+
+        #lê os dados enquento eles sao enviados
+        while True:
+            data = conn.recv(packetSize)
+            data_received += len(data)
+            #print(data_received)
+            if not data:
+                tempo_final = time.time() - tempo_inicial
+                break
+            #print(f'Server sent: {conn.send(data)} bytes')
+        print(f'All data received.\nReceived {data_received} bytes in {tempo_final} seconds')
+        return tempo_final
+
+if __name__ == "__main__":
+    servidorTCP(HOST, PORT, PACKET_SIZE)
